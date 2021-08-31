871c93e98342a39c614da54c902749c4e11fb77e
Matt Winter <winter5187@gmail.com>
2021-08-30 21:51:59 -0500

    local storage js

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+// Global Variables
+    // Today's Date
+
+// Functions
+    // Time Tracker Function
+
+
+// Local Storage
+$("8am .input").val(localStorage.getItem("8am"));
+$("9am .input").val(localStorage.getItem("9am"));
+$("10am .input").val(localStorage.getItem("10am"));
+$("11am .input").val(localStorage.getItem("11am"));
+$("12pm .input").val(localStorage.getItem("12pm"));
+$("1pm .input").val(localStorage.getItem("1pm"));
+$("2pm .input").val(localStorage.getItem("2pm"));
+$("3pm .input").val(localStorage.getItem("3pm"));
+$("4pm .input").val(localStorage.getItem("4pm"));
+$("5pm .input").val(localStorage.getItem("5pm"));
+
+
+
+
+
+
+
+
+
+
+// Buttons/Listeners 
+saveBtn.on("click", function() {
+
+})
